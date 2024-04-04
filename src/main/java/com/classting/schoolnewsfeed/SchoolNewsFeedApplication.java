b3754a4af3a2b91f92cd20b597923dd2c931f25b
Carrotww <rjgjfl90@naver.com>
2024-04-04 14:35:25 +0900

    project init

---
+++ src/main/java/com/classting/schoolnewsfeed/SchoolNewsFeedApplication.java
@@ -0,0 +1,13 @@
+package com.classting.schoolnewsfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolNewsFeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolNewsFeedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/classting/schoolnewsfeed/SchoolNewsFeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.classting.schoolnewsfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolNewsFeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
